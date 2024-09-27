1c71dde3a6aa70d9d682049952e1b71f25f78af2
Ellie A Sager <34947031+ellieasager@users.noreply.github.com>
2024-09-26 19:51:53 -0700

    another problem

---
+++ leet-java/src/main/java/com/unicats/solutions/solutions27/RemoveElement.java
@@ -0,0 +1,52 @@
+package com.unicats.solutions.solutions27;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+      System.out.println(new RemoveElement().removeElement(new int[]{3,2,2,3}, 3));
+    }
+
+  public int removeElement(int[] nums, int val) {
+    int k = 0;
+    for (int x : nums) {
+        if (x != val) {
+            nums[k++] = x;
+        }
+    }
+    return k;
+  }
+
+  public int removeElementEllie(int[] nums, int val) {
+    int currentPostion = 0;
+    
+    while (currentPostion < nums.length) {
+      if (nums[currentPostion] == val) {
+        boolean foundGoodPlace = false;
+        int insertPosition = currentPostion;
+        while (insertPosition < nums.length - 1 && !foundGoodPlace) {
+          insertPosition++;
+          if (nums[insertPosition] != val) 
+          { 
+            foundGoodPlace = true;
+            break;
+          }
+        }
+        if (foundGoodPlace) swap(nums, currentPostion, insertPosition); 
+        else break;
+      }
+      currentPostion++;
+    }
+    return currentPostion;
+  }
+
+  void swap(int[] nums, int a, int b) {
+    int tmp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = tmp;
+  }
+
+  // int tmp = nums[currentPostion];
+  // nums[currentPostion] = nums[insertPosition];
+  // nums[insertPosition] = tmp;
+  
+}
